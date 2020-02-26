a63971e8fc0170e64c6a5124d0ad06121da48555
Samuel Krut <sam@MacBook-Pro-6.local>
2020-02-25 23:01:40 -0500

    first package working

--- BLB/R/hello.R
+++ BLB/R/hello.R
@@ -1,18 +1,32 @@
- # Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+#' Test function
+#' @export
+test <- function() {
 
-hello <- function() {
-  print("Hello, world!")
+  library(nycflights13)
+  set.seed(141)
+  m <- 10
+  groups <- sample(seq_len(m), nrow(flights), replace = TRUE)
+  dir.create("flights/", showWarnings = FALSE)
+  for (i in seq_len(m)) {
+    write_csv(filter(flights, groups == i), str_c("flights/", i, ".csv"))
+  }
+
+  file_names <- file.path("flights", list.files("flights"))
+  mean_list <- file_names %>% map(~ mean(read_csv(.)$dep_delay, na.rm = TRUE))
+  (mean_dep_delay <- mean_list %>% reduce(`+`) / m)
+
+  ci_list <- file_names %>% map(~ t.test(read_csv(.)$dep_delay)$conf.int)
+  (mean_ci <- ci_list %>% reduce(`+`) / m)
+
+  r <- 10  # r should be at least a few thousands, we are using 10 for demo
+  n <- nrow(flights)
+  each_boot <- function(i, data) {
+    mean(sample(data, n, replace = TRUE), na.rm = TRUE)
+  }
+  ci_list <- file_names %>% map(~ {
+    sub_dep_delay <- read_csv(.)$dep_delay
+    map_dbl(seq_len(r), each_boot, data = sub_dep_delay) %>%
+      quantile(c(0.025, 0.975))
+  })
+  reduce(ci_list, `+`) / length(ci_list)
 }
