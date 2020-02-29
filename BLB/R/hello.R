8a397726663995132dbbbd6edd5233ae6a7e4f97
chriztopherton <christopherton1998@gmail.com>
2020-02-29 15:22:37 -0800

    took out lines

--- BLB/R/hello.R
+++ BLB/R/hello.R
@@ -5,23 +5,25 @@ blb <- function(processing_type) {
   library(nycflights13)
   library(tidyverse)
 
-  set.seed(141)
-  m <- 10
-  groups <- sample(seq_len(m), nrow(flights), replace = TRUE)
-  dir.create("flights/", showWarnings = FALSE)
-  for (i in seq_len(m)) {
-    write_csv(filter(flights, groups == i), str_c("flights/", i, ".csv"))
-  }
+  # set.seed(141)
+  # m <- 10
+  # groups <- sample(seq_len(m), nrow(flights), replace = TRUE)
+  # dir.create("flights/", showWarnings = FALSE)
+  # for (i in seq_len(m)) {
+  #   write_csv(filter(flights, groups == i), str_c("flights/", i, ".csv"))
+  # }
 
   file_names <- file.path("flights", list.files("flights"))
-  mean_list <- file_names %>% map(~ mean(read_csv(.)$dep_delay, na.rm = TRUE))
-  (mean_dep_delay <- mean_list %>% reduce(`+`) / m)
+  #mean_list <- file_names %>% map(~ mean(read_csv(.)$dep_delay, na.rm = TRUE))
+  #(mean_dep_delay <- mean_list %>% reduce(`+`) / m)
 
-  ci_list <- file_names %>% map(~ t.test(read_csv(.)$dep_delay)$conf.int)
-  (mean_ci <- ci_list %>% reduce(`+`) / m)
+  #ci_list <- file_names %>% map(~ t.test(read_csv(.)$dep_delay)$conf.int)
+  #(mean_ci <- ci_list %>% reduce(`+`) / m)
 
   r <- 10  # r should be at least a few thousands, we are using 10 for demo
   n <- nrow(flights)
+  
+  # generated subsamples using multinomial dist
   
   # each_boot <- function(i, data) {
   #   mean(sample(data, n, replace = TRUE), na.rm = TRUE)
