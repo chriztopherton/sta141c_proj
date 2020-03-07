88bfd1a11f05a6fd8b27176e6f8ef4a548f00e46
chriztopherton <christopherton1998@gmail.com>
2020-03-07 11:43:45 -0800

    R files

---
+++ BLB/R/sigma_ci.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(readr)
+
+sigma_ci <- function(data){
+  set.seed(141)
+  m <- 10
+  n <- nrow(data)
+  group <- sample(seq_len(m), n, replace = TRUE)
+  map(seq_len(m), ~write_csv(data[group ==., ],str_c("part",.,".csv")))
+
+  calc_slope_blb <- function(subsample, freqs){
+    as.numeric(summary(lm(y ~ ., weights = freqs, data = subsample))["sigma"])
+  }
+
+  library(furrr)
+  plan(multiprocess, workers = 4)
+  B <- 10
+
+  each_boot <-function(i, ind_part){
+    subsample <- read_csv(str_c("part", ind_part,".csv"))
+    freqs <- rmultinom(1, n, rep(1, nrow(subsample)))
+    calc_slope_blb(subsample, freqs)
+  }
+
+  ci_list <- future_map(seq_len(m), ~{
+    map_dbl(seq_len(B), each_boot, ind_part =.) %>%
+      quantile(c(0.025,0.975))
+  })
+  reduce(ci_list, `+`)/length(ci_list)
+}
